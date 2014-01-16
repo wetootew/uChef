5693e472098289a0c316e3a8058f8111a3fc0fce
wetootew <x.x@aol.it>
2014-01-16 21:57:08 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-echo '<p>db link not impemented!</p>' //if (!pg_connect("dbname=d6qm36u34n7hb7 host=ec2-107-20-234-127.compute-1.amazonaws.com port=5432 user=ctgwllpexesxtg password=SnntH9C3QrH1TgArkLb0ytXlm- sslmode=require"))
+echo '<p>db link not impemented!</p>'; //if (!pg_connect("dbname=d6qm36u34n7hb7 host=ec2-107-20-234-127.compute-1.amazonaws.com port=5432 user=ctgwllpexesxtg password=SnntH9C3QrH1TgArkLb0ytXlm- sslmode=require"))
     //echo '<p> db error </p>';
 ?>
