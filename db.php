33ea9579898bf1e941b2cb52a39dcbaf8f394a0e
wetootew <x.x@aol.it>
2014-02-13 15:54:18 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
 if (!mysql_select_db('uchef', $link = mysql_connect('uchef.mysql.eu1.frbit.com', 'uchef', 'WY6ioOmkHkUd4zMU')))
-    echo '<p> db error ' . mysql_errno() . ": " . mysql_error() . '</p>;
+    echo '<p> db error ' . mysql_errno() . ": " . mysql_error() . '</p>';
 ?>
